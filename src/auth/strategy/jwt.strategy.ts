bbdb3c87dcc01ff957c9a240b59f0710c96c535a
Jayesh Sadhwani <jayeshsadhwani99@gmail.com>
2022-07-29 13:56:11 +0530

    Basic data

---
+++ src/auth/strategy/jwt.strategy.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { auth } from 'firebase-admin';
+import { DecodedIdToken } from 'firebase-admin/lib/auth/token-verifier';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable({})
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor(private prisma: PrismaService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    });
+  }
+
+  async validate(payload: string) {
+    const decodedIdToken: DecodedIdToken = await auth().verifyIdToken(payload);
+
+    const user = this.prisma.user.findOne({
+      where: {
+        firebase_uid: decodedIdToken.uid,
+      },
+    });
+
+    return user;
+  }
+}

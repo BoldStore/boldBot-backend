f509fb6f109ba82e28a6389bb2bff8766ec63986
Jayesh Sadhwani <jayeshsadhwani99@gmail.com>
2022-08-04 23:49:55 +0530

    Enable logs

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,10 @@ import { AppModule } from './app.module';
 import * as admin from 'firebase-admin';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+    logger: ['error', 'warn', 'log'],
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({

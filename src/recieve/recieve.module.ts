340c8e25a3cbbd179fdd742ebfdc70f675bc5f60
Jayesh Sadhwani <jayeshsadhwani99@gmail.com>
2022-08-21 00:49:13 +0530

    Fix errors

--- src/recieve/recieve.module.ts
+++ src/recieve/recieve.module.ts
@@ -1,9 +1,10 @@
 import { Global, Module } from '@nestjs/common';
+import { RecieveHelpers } from './recieve.helpers';
 import { RecieveService } from './recieve.service';
 
 @Global()
 @Module({
-  providers: [RecieveService],
+  providers: [RecieveService, RecieveHelpers],
   exports: [RecieveService],
 })
 export class RecieveModule {}

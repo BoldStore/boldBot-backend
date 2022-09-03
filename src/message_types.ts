e0aa69eda66c65bbc7be93485482bfac4563d92e
Jayesh Sadhwani <jayeshsadhwani99@gmail.com>
2022-09-03 15:10:01 +0530

    proper response, optimized code get stats

--- src/message_types.ts
+++
@@ -1,5 +0,0 @@
-export const GREETING = 'greeting';
-export const ICE_BREAKER = 'ice-breaker';
-export const PERSISTENT_MENU = 'persistent-menu';
-export const STORY_REPLY = 'story-reply';
-export const STORY_MENTION = 'story-mention';
--- src/stats/stats.service.ts
+++ src/stats/stats.service.ts
@@ -1,64 +1,48 @@
 import { Injectable } from '@nestjs/common';
 import { User } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
-import * as MessageTypes from '../message_types';
 
 @Injectable()
 export class StatsService {
   constructor(private prisma: PrismaService) {}
 
   async getStats(user: User, page_id: string) {
-    const greetings: number = await this.prisma.messageCount.count({
+    const count = await this.prisma.messageCount.groupBy({
+      by: ['serviceId'],
+      _count: true,
       where: {
         userId: user.id,
         pageId: page_id,
-        service: {
-          name: MessageTypes.GREETING,
-        },
       },
     });
-    const ice_breakers: number = await this.prisma.messageCount.count({
-      where: {
-        userId: user.id,
-        pageId: page_id,
-        service: {
-          name: MessageTypes.ICE_BREAKER,
-        },
-      },
-    });
-    const persistent_menu: number = await this.prisma.messageCount.count({
-      where: {
-        userId: user.id,
-        pageId: page_id,
-        service: {
-          name: MessageTypes.PERSISTENT_MENU,
-        },
-      },
-    });
-    const story_replies: number = await this.prisma.messageCount.count({
-      where: {
-        userId: user.id,
-        pageId: page_id,
-        service: {
-          name: MessageTypes.STORY_REPLY,
-        },
-      },
-    });
-    const story_mentions: number = await this.prisma.messageCount.count({
-      where: {
-        userId: user.id,
-        pageId: page_id,
-        service: {
-          name: MessageTypes.STORY_MENTION,
-        },
-      },
-    });
+
+    const services = [];
+
+    for (let i = 0; i < count.length; i++) {
+      const serviceId = count[i].serviceId;
+      services.push(
+        await this.prisma.services.findFirst({
+          where: {
+            id: serviceId,
+          },
+          select: {
+            name: true,
+            id: true,
+          },
+        }),
+      );
+    }
+
     return {
-      greetings,
-      ice_breakers,
-      persistent_menu,
-      story_mentions,
-      story_replies,
+      count: count.map((e) => {
+        const service = services.find(
+          (service_data) => service_data.id == e.serviceId,
+        );
+        return {
+          service: service.name,
+          count: e._count,
+        };
+      }),
     };
   }
 }

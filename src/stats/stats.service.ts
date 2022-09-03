bf5d25e51d376414809762bfac0f4949c681ba33
Jayesh Sadhwani <jayeshsadhwani99@gmail.com>
2022-09-03 14:14:50 +0530

    get Stats

--- src/stats/stats.service.ts
+++ src/stats/stats.service.ts
@@ -1,17 +1,64 @@
 import { Injectable } from '@nestjs/common';
 import { User } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
+import * as MessageTypes from '../message_types';
 
 @Injectable()
 export class StatsService {
+  constructor(private prisma: PrismaService) {}
+
   async getStats(user: User, page_id: string) {
-    // TODO: Get greetings
-    // TODO: Get ice-breakers
-    // TODO: Get persistent-menu
-    // TODO: Get story replies
-    // TODO: Get story mentions
+    const greetings: number = await this.prisma.messageCount.count({
+      where: {
+        userId: user.id,
+        pageId: page_id,
+        service: {
+          name: MessageTypes.GREETING,
+        },
+      },
+    });
+    const ice_breakers: number = await this.prisma.messageCount.count({
+      where: {
+        userId: user.id,
+        pageId: page_id,
+        service: {
+          name: MessageTypes.ICE_BREAKER,
+        },
+      },
+    });
+    const persistent_menu: number = await this.prisma.messageCount.count({
+      where: {
+        userId: user.id,
+        pageId: page_id,
+        service: {
+          name: MessageTypes.PERSISTENT_MENU,
+        },
+      },
+    });
+    const story_replies: number = await this.prisma.messageCount.count({
+      where: {
+        userId: user.id,
+        pageId: page_id,
+        service: {
+          name: MessageTypes.STORY_REPLY,
+        },
+      },
+    });
+    const story_mentions: number = await this.prisma.messageCount.count({
+      where: {
+        userId: user.id,
+        pageId: page_id,
+        service: {
+          name: MessageTypes.STORY_MENTION,
+        },
+      },
+    });
     return {
-      user,
-      page_id,
+      greetings,
+      ice_breakers,
+      persistent_menu,
+      story_mentions,
+      story_replies,
     };
   }
 }

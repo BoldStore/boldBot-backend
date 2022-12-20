680f01aebc063195ff2ab38e26e73cdde169c985
aryanteng <aryanteng892@gmail.com>
2022-12-20 20:41:34 +0530

    contact dto

--- src/contact/dto/contact.dto.ts
+++ src/contact/dto/contact.dto.ts
@@ -1,10 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class ContactDto {
+  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   name: string;
 
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   insta_username: string;

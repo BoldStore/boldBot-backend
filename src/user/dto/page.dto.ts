681b1d69a48b20f5625f4082a96ced5395e2b22f
aryanteng <aryanteng892@gmail.com>
2022-12-20 20:42:14 +0530

    page dto

--- src/user/dto/page.dto.ts
+++ src/user/dto/page.dto.ts
@@ -1,18 +1,23 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class PageDto {
+  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   access_token: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   data_access_expiration_time: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   expires_in: string;
 
+  @ApiProperty()
   @IsOptional()
   @IsString()
   long_lived_token?: string;

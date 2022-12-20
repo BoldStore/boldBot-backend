e1257f470f2670deebfe0644466eac4363a85241
aryanteng <aryanteng892@gmail.com>
2022-12-20 20:44:08 +0530

    ice breaker dto

--- src/message/dto/ice-breaker.dto.ts
+++ src/message/dto/ice-breaker.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 interface Text {
@@ -11,9 +12,11 @@ interface IceBreaker {
 }
 
 export class IceBreakerDto {
+  @ApiProperty()
   @IsString()
   pageId: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   ice_breakers: IceBreaker[];
 }
